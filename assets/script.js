f99edc97fed6732c0e890a7cc43d5933b07eddb1
samergain <samersadon@gmail.com>
2020-09-28 01:30:01 -0400

    adding eventListner to btns. the for loops need work and setting the timer

--- assets/script.js
+++ assets/script.js
@@ -17,75 +17,43 @@ var btn3El = document.getElementById("btn3");
 var btn4El = document.getElementById("btn4");
 var btns = [btn1El, btn2El, btn3El, btn4El];
 var pEl = document.getElementById("subResult");
-//timer start
-//print question with answers
-for (var i=0; i<5; i++) {
-    
-    h3q1.textContent = questions[i];
-    var correctAnswer = answerSet[i].correct;
-    console.log("correctAnswer: " + correctAnswer);
-    for(var j=0; j<4; j++) {
-        btns[j].textContent = answerSet[i].answers[j];
-        if (j == correctAnswer) {
-        btns[j].setAttribute("bingo","true");
-        }
-    }
-    answersDiv.addEventListener("click", function(event) {
-        event.preventDefault();
-        if(event.target.matches("button")) {
-            // if (bingo is true) {
-                var pressedBtn = event.target;
-                if (pressedBtn.bingo = "true") {
-                    pEl.textContent = "Correct!";
-                    //delay for 1 second
-                } else {
-                    //timer -10
-                    pEl.textContent = "Wrong!";
-                    //delay for 1 second
+function startQuiz() {
+        //timer start
+        
+        //print question with answers
+        for (var i=0; i<5; i++) {
+            h3q1.textContent = questions[i];
+            var correctAnswer = answerSet[i].correct;
+            console.log("correctAnswer: " + correctAnswer);
+            //fill in the answers and set bingo to true for the correct answer
+            for(var j=0; j<4; j++) {
+                btns[j].textContent = answerSet[i].answers[j];
+                if (j == correctAnswer) {
+                btns[j].setAttribute("bingo","true");
                 }
-
-            
-
-       
-       
+            }
+            answersDiv.addEventListener("click", function(event) {
+                event.preventDefault();
+                if(event.target.matches("button")) {
+                    // if (bingo is true) {
+                        var pressedBtn = event.target;
+                        if (pressedBtn.bingo = "true") {
+                            pEl.textContent = "Correct!";
+                            //delay for 1 second
+                            setTimeout(()=>{},1000);
+                        } else {
+                            //timer -10
+                            pEl.textContent = "Wrong!";
+                            //delay for 1 second
+                            setTimeout(()=>{},1000);
+                        } 
+                // try to replace the loop with i++; here and repeat        
+                }
+            });
         }
-    });
+
 }
 
 
 
-
-
 //timer end
-
-
-
-
-
-
-
-
-
-//   // We start the game with a score of 0.
-//   var score = 0;
-
-//   // Loop over every question object
-//   for (var i = 0; i < questions.length; i++) {
-//     // Display current question to user and ask OK/Cancel
-//     var answer = confirm(questions[i].q);
-    
-
-//     // Compare answers
-//     if ((answer === true && questions[i].a === "t") ||
-//       (answer === false && questions[i].a === "f")) {
-//       // Increase score
-//       score++;
-//       alert("Correct!");
-//     }
-//     else {
-//       alert("Wrong!");
-//     }
-//   }
-
-//   // Show total at end
-//   alert("You got " + score + "/" + questions.length);
